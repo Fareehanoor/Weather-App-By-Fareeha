c39e51fd77a8dadfff59199c6a1a29c483db30c7
Fareeha Abbas <141918326+Fareehanoor@users.noreply.github.com>
2023-11-02 09:48:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+
+        const apiKey="2f8d5e7d94b677386555f407c0018ff7";
+        const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+        
+        const searchBox = document.querySelector(".search input");
+        const searchBtn = document.querySelector(".search button");
+        const weatherIcon = document.querySelector(".weather-icon");
+        async function checkWeather(city){
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+            if(response.status == 404){
+                document.querySelector(".error").style.display = "block";
+                document.querySelector(".weather").style.display = "none";
+            }
+            else{
+                var data=await response.json();
+
+                document.querySelector(".city").innerHTML = data.name;
+                document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+                
+                document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+                document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    
+                if(data.weather[0].main == "Clouds"){
+                    weatherIcon.src="clouds.png";
+                }
+                else if(data.weather[0].main == "Clear"){
+                    weatherIcon.src="clear.png";
+                }
+                else if(data.weather[0].main == "Rain"){
+                    weatherIcon.src="rain.png";
+                }
+                else if(data.weather[0].main == "Drizzle"){
+                    weatherIcon.src="drizzle.png";
+                }
+                else if(data.weather[0].main == "Mist"){
+                    weatherIcon.src="mist.png";
+                }
+    
+                document.querySelector(".weather").style.display="block";
+                document.querySelector(".error").style.display = "none";
+                
+                
+            }   
+            
+        }    
+            
+           searchBtn.addEventListener("click" , ()=>{
+            checkWeather(searchBox.value);
+        })
+
+    
+    
